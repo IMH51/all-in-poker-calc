e43baadeff4bd417615b1487be2047d6f20023ea
IMH51 <ianmartinharrison@outlook.com>
2022-01-08 12:53:15 +0000

    Installed theme-ui, added basic theme object

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
+/** @jsxImportSource theme-ui */
 import React from 'react';
 import { render } from 'react-dom';
+import { ThemeProvider } from '@theme-ui/core';
+import { theme } from './theme-ui';
 
 const appRoot = document.getElementById('root');
 
-render(<div>Root Element</div>, appRoot);
+render(
+    <ThemeProvider theme={theme}>
+        <div sx={{ color: 'text', background: 'background' }}>Root Element</div>
+    </ThemeProvider>,
+    appRoot,
+);
