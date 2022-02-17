190b9b6c35a1b8153ebf6778279eb6a76be5139a
IMH51 <ianmartinharrison@outlook.com>
2022-02-17 08:59:40 +0000

    Added extra viewports to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,15 @@
 import { Providers } from '../src/components/Providers';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+console.log(INITIAL_VIEWPORTS);
 
 export const decorators = [(story) => <Providers>{story()}</Providers>];
 
 export const parameters = {
+    viewport: {
+        viewports: INITIAL_VIEWPORTS,
+        defaultViewport: 'iphone12',
+    },
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
         matchers: {
